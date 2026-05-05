793733ad6d91718d72357dbfb2dc6465a256bc79
Андрей <keikuethas@yandex.ru>
2026-05-05 10:51:57 +0300

    Removed GeneralScreen

--- app/src/main/java/com/keikuethas/irlhideandseek/MainActivity.kt
+++ app/src/main/java/com/keikuethas/irlhideandseek/MainActivity.kt
@@ -5,18 +5,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.CenterAlignedTopAppBar
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
 import com.keikuethas.irlhideandseek.ui.theme.IRLHideAndSeekTheme
 import com.keikuethas.irlhideandseek.view.AppNavigation
 
@@ -26,7 +14,6 @@ class MainActivity : ComponentActivity() {
 
         MapKitInitializer.initialize(this)
 
-        //vibecode
         // Запрос разрешений на геолокацию
         requestPermissions(
             arrayOf(
@@ -39,31 +26,13 @@ class MainActivity : ComponentActivity() {
 
         enableEdgeToEdge()
         setContent {
-            GeneralScreen { innerPadding -> AppNavigation(innerPadding) }
+            IRLHideAndSeekTheme {
+                AppNavigation()
+            }
         }
     }
 
-    //часть vibecode
     companion object {
         private const val LOCATION_PERMISSION_REQUEST_CODE = 1
     }
 }
-
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-// Эта функция нужна, чтобы задать общие настройки типа topBar и bottomBar
-// Удобно для предпросмотра
-fun GeneralScreen(screen: @Composable (PaddingValues) -> Unit) {
-    IRLHideAndSeekTheme {
-        Scaffold(
-            modifier = Modifier.fillMaxSize().background(Color.Blue),
-            topBar = { CenterAlignedTopAppBar(title = {Box {Text("Hide and Seek")}}) },
-        ) { innerPadding ->
-            screen(innerPadding)
-        }
-    }
-}
-
-@Preview
-@Composable
-fun GeneralScreenPreview() = GeneralScreen { }
