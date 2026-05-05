bd6d6036730b5bf5bbc8b7692055eceb729527ef
Андрей <keikuethas@yandex.ru>
2026-05-05 10:55:25 +0300

    Changed role as a concept

--- app/src/main/java/com/keikuethas/irlhideandseek/PlayerRole.kt
+++ app/src/main/java/com/keikuethas/irlhideandseek/PlayerRole.kt
@@ -1,7 +1,30 @@
 package com.keikuethas.irlhideandseek
 
-import java.util.UUID
+import kotlinx.serialization.Serializable
 
-enum class PlayerRole {
-    Hider, Seeker
+@Serializable
+data class PlayerRole(
+    val name: String,
+    val abilities: List<Ability> = listOf(),
+    val type: RoleType
+) {
+
+}
+
+enum class RoleType {
+    Hider, Seeker;
+
+    // resource
+    override fun toString(): String = when(this) {
+        Hider -> "Прячущийся"
+        Seeker -> "Охотник"
+    }
+
+    // resource
+    val description: String by lazy {
+        when (this) {
+            Hider -> "Задача игроков с ролью этого типа - оставаться в живых, пока не истечёт время игры."
+            Seeker -> "Задача игроков с ролью этого типа - найти всех прячущихся до истечения времени игры."
+        }
+    }
 }
