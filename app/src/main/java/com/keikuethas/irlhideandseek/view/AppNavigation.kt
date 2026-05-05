8a694d15497741318dd0bf2112975940f726242b
Андрей <keikuethas@yandex.ru>
2026-05-05 10:50:52 +0300

    Changed NavController

--- app/src/main/java/com/keikuethas/irlhideandseek/view/AppNavigation.kt
+++ app/src/main/java/com/keikuethas/irlhideandseek/view/AppNavigation.kt
@@ -3,37 +3,39 @@ package com.keikuethas.irlhideandseek.view
 import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.runtime.Composable
-import com.google.accompanist.navigation.animation.rememberAnimatedNavController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import androidx.navigation.toRoute
+import com.google.accompanist.navigation.animation.rememberAnimatedNavController
 import com.keikuethas.irlhideandseek.view.game.GameScreen
 import com.keikuethas.irlhideandseek.view.lobby.LobbyScreen
 import com.keikuethas.irlhideandseek.view.settings_screens.AbilitiesSettingsScreen
 import com.keikuethas.irlhideandseek.view.settings_screens.LobbySettingsScreen
 import com.keikuethas.irlhideandseek.view.settings_screens.MapSettingsScreen
-import com.keikuethas.irlhideandseek.view.settings_screens.RolesSettingsScreen
 import com.keikuethas.irlhideandseek.view.settings_screens.RulesSettingsScreen
 
 @OptIn(ExperimentalAnimationApi::class)
 @Composable
-fun AppNavigation(innerPadding: PaddingValues) {
-    val navController = rememberAnimatedNavController()
+fun AppNavigation() {
+    val navController = rememberNavController()
 
     NavHost(navController, startDestination = Home) {
 
         composable<Home> {
-            HomeScreen(innerPadding, navController)
+            HomeScreen(navController)
         }
 
         composable<Lobby>{
-            LobbyScreen(innerPadding, navController)
+            val playerName = it.toRoute<Lobby>().playerName
+            val roomName = it.toRoute<Lobby>().roomName
+            LobbyScreen(navController, playerName, roomName)
         }
 
         //---
-        composable<RolesSettings> {
-            RolesSettingsScreen()
-        }
+//        composable<RolesSettings> {
+//            RolesSettingsScreen(it.toRoute<RolesSettings>().gameSettings)
+//        } //TEMP
 
         composable<RulesSettings>  {
             RulesSettingsScreen()
@@ -53,8 +55,8 @@ fun AppNavigation(innerPadding: PaddingValues) {
         //---
 
         composable<Game> { backStackEntry ->
-            val role = backStackEntry.toRoute<Game>().role
-            GameScreen(innerPadding, navController)
+            //val role = backStackEntry.toRoute<Game>().role
+            GameScreen(navController)
         }
     }
 }
