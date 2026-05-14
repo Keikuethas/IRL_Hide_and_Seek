a74a64ebde5dcf75c0491703ddea37d5fc773107
Андрей <keikuethas@yandex.ru>
2026-05-14 09:20:58 +0300

    Migrated to MVI_HiltViewModel, LaunchedEffect moved out of container

--- app/src/main/java/com/keikuethas/irlhideandseek/view/HomeScreen.kt
+++ app/src/main/java/com/keikuethas/irlhideandseek/view/HomeScreen.kt
@@ -38,6 +38,7 @@ import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import com.keikuethas.irlhideandseek.mvi.home.HomeEffect
 import com.keikuethas.irlhideandseek.mvi.home.HomeIntent
+import com.keikuethas.irlhideandseek.mvi.home.HomeState
 import com.keikuethas.irlhideandseek.mvi.home.HomeViewModel
 
 // refactor вынести строки в ресурсы
@@ -50,9 +51,10 @@ fun HomeScreen(
     val context = LocalContext.current
 
     // состояние экрана
-    val state = homeViewModel.homeState.collectAsStateWithLifecycle()
+    val state = homeViewModel.state.collectAsStateWithLifecycle()
 
     // refactor наверняка это должно быть не здесь
+    //  идея: синглтон-класс, который распоряжается всей геолокацией
     // есть ли доступ к геолокации
     val permissionsGranted = remember(context) {
         ContextCompat.checkSelfPermission(
@@ -156,11 +158,109 @@ fun HomeScreen(
             }
         }
     }
+
+    LaunchedEffect(key1 = Unit) {
+        homeViewModel.effect.collect { effect ->
+            when (effect) {
+                HomeEffect.OpenSettings -> {
+                    Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS).apply {
+                        data = Uri.fromParts("package", context.packageName, null)
+                    }.let { context.startActivity(it) }
+                }
+
+                HomeEffect.HostLobby -> TODO()
+                is HomeEffect.JoinLobby -> navController.navigate(
+                    Lobby(
+                        state.value.nameText,
+                        effect.id
+                    )
+                )
+            }
+        }
+    }
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Preview
 @Composable
-fun HomePreview() {
-    HomeScreen()
+fun HomeScreenUI(
+    state: HomeState = HomeState(),
+    onIntent: (HomeIntent) -> Unit = {},
+    permissionsGranted: Boolean = true //temp
+) {
+    Scaffold(
+        topBar = { CenterAlignedTopAppBar(title = {Box {Text("Hide and Seek")}}) }
+    ) { innerPadding ->
+        state.error?.let { error ->
+            ErrorDialog(
+                title = error.title,
+                description = error.description
+            ) { onIntent(HomeIntent.DismissError) }
+        }
+
+        if (permissionsGranted) {
+            Column(
+                Modifier
+                    .padding(innerPadding)
+                    .fillMaxSize()
+                    .padding(vertical = 100.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                OutlinedTextField(
+                    value = state.nameText,
+                    onValueChange = { value -> onIntent(HomeIntent.EditName(value)) },
+                    label = {
+                        Row {
+                            Text("Отображаемое имя")
+                            Spacer(Modifier.width(10.dp))
+                            Icon(Icons.Default.Person, contentDescription = null)
+                        }
+                    },
+                    modifier = Modifier.padding(top = 10.dp, bottom = 10.dp),
+                    supportingText = { Text(state.nameTextCounter) }
+                )
+
+                OutlinedTextField(
+                    state.roomNameText,
+                    onValueChange = { value -> onIntent(HomeIntent.EditRoomName(value)) },
+                    label = {
+                        Row {
+                            Text("Идентификатор комнаты")
+                            Spacer(Modifier.width(10.dp))
+                            Icon(Icons.Default.LocationOn, contentDescription = null)
+                        }
+                    },
+                    modifier = Modifier.padding(top = 10.dp, bottom = 10.dp),
+                    supportingText = { Text(state.roomNameTextCounter) }
+                )
+
+                ElevatedButton(
+                    onClick = { onIntent(HomeIntent.JoinGame) },
+                    enabled = state.buttonsActive
+                ) { Text("Присоединиться") }
+
+                ElevatedButton(
+                    onClick = { onIntent(HomeIntent.CreateGame) },
+                    enabled = state.buttonsActive
+                ) { Text("Создать") }
+            }
+        } else {
+            Column(
+                Modifier
+                    .fillMaxSize()
+                    .padding(innerPadding)
+                    .padding(horizontal = 50.dp),
+                verticalArrangement = Arrangement.spacedBy(20.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text( //resource
+                    "Игровой процесс построен на работе с Вашим местоположением в реальной жизни. " +
+                            "Пожалуйста, предоставьте разрешение на работу с геолокацией."
+                )
+                ElevatedButton(
+                    onClick = { onIntent(HomeIntent.GrantPermissions) },
+                ) { Text("Предоставить разрешение") }
+            }
+        }
+    }
 }
