88c06031ee66a2bed60b9e8437c133e99d4bcbbc
Андрей <keikuethas@yandex.ru>
2026-05-18 14:53:41 +0300

    Added full game settings structure

--- app/src/main/java/com/keikuethas/irlhideandseek/GameSettings.kt
+++ app/src/main/java/com/keikuethas/irlhideandseek/GameSettings.kt
@@ -1,15 +1,50 @@
 package com.keikuethas.irlhideandseek
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.buildJsonObject
+import kotlinx.serialization.json.put
 
-//DESCRIBE: описание должно обновляться по мере обновления класса
+@Serializable
+@Parcelize
+data class RoleParams(
+    val health: Int = 100,
+    val victory_condition: RoleType
+): Parcelable
+
+//DESCRIBE
 /**
- * Класс, хранящий информацию о всех настройках игры (комнаты).
- * @param id Имя комнаты
- * @property playersCanChangeRole Может ли игрок изменять свою роль
+ * Настройки игры. Отправляются на сервер при создании комнаты
  */
-data class GameSettings(val id: String, val hostId: String) {
-    // лобби
-    var playersCanChangeRole: Boolean = true
+@Serializable
+data class GameSettings(
+    val name: String = "New game",
+    val center_lat: Double = 0.0,
+    val center_lng: Double = 0.0,
+    val safe_zone_radius: Double = 500.0,
+    val min_zone_radius: Double = 500.0,
+    val zone_shrink_interval: Int = 120,
+    val game_duration: Int = 1800,
+    val time_to_hide: Int = 300,
 
-    // игра
+    val host_name: String = "host name",
+    val host_player_location_lat: Double = 0.0,
+    val host_player_location_lng: Double = 0.0,
+
+    val game_roles: Map<String, RoleParams> = emptyMap(),
+
+    val roles_abilities: Map<String, Map<String, Ability>> = emptyMap(),
+
+    val events_configurations: Map<String, Event> = emptyMap(),
+    val roles_events: Map<String, List<String>> = emptyMap()
+    ) {
+
+    val host_player get() = buildJsonObject {
+        put("host_name", host_name)
+        put("host_player_location_lat", host_player_location_lat)
+        put("host_player_location_lng", host_player_location_lng)
+    }
+
+    val events get() = events_configurations.keys.toList()
 
 }
