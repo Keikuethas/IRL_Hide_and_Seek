85bffd2f7e5a47d55775ac9a83d9a3b5351f3b4e
Андрей <keikuethas@yandex.ru>
2026-05-18 14:53:11 +0300

    WIP

--- app/src/main/java/com/keikuethas/irlhideandseek/view/NewGameScreen.kt
+++ app/src/main/java/com/keikuethas/irlhideandseek/view/NewGameScreen.kt
@@ -7,16 +7,29 @@ import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-//import com.keikuethas.irlhideandseek.utils.LocationTracker
-import com.keikuethas.irlhideandseek.view.lobby.SettingsButtons
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+import com.keikuethas.irlhideandseek.mvi.newGame.main.NewGameEffect
+import com.keikuethas.irlhideandseek.mvi.newGame.main.NewGameViewModel
+import com.keikuethas.irlhideandseek.utils.LocationTracker
+import com.keikuethas.irlhideandseek.view.newgame.SettingsButtons
 
 @Composable
-fun NewGameScreen() {
+fun NewGameScreen(
+    navController: NavController = rememberNavController(),
+    newGameViewModel: NewGameViewModel = viewModel()
+    ) {
 
-//    SettingsButtons(navController)
+    val state = newGameViewModel.state.collectAsStateWithLifecycle()
+
+    SettingsButtons(navController)
 
     Spacer(Modifier.height(20.dp))
 
@@ -42,15 +55,30 @@ fun NewGameScreen() {
         Text("Отмена")
     }
 
-//    //геолокация refactor отсюда в viewModel
-//    val locationTracker = LocationTracker(LocalContext.current)
-//    var host_lat: Double = 0.0
-//    var host_lng: Double = 0.0
-//    locationTracker.getCurrentLocation(
-//        onSuccess = { latitude, longitude ->
-//            host_lat = latitude
-//            host_lng = longitude
-//        },
-//        onError = {  } //todo обработчик
-//    )
+    //геолокация refactor отсюда в viewModel и дальше
+    val locationTracker = LocationTracker(LocalContext.current)
+    var host_lat: Double = 0.0
+    var host_lng: Double = 0.0
+    locationTracker.getCurrentLocation(
+        onSuccess = { latitude, longitude ->
+            host_lat = latitude
+            host_lng = longitude
+        },
+        onError = {  } //todo обработчик
+    )
+
+    LaunchedEffect(key1 = Unit) {
+        newGameViewModel.effect.collect { effect ->
+            when(effect) {
+                is NewGameEffect.JoinGame -> TODO()
+                NewGameEffect.Quit -> TODO()
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun PreviewNewGame() {
+    NewGameScreen()
 }
