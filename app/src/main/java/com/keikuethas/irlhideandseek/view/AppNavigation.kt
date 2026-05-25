6d364d0cdd5ab62d9ea757954e5db4a9fe571663
Андрей <keikuethas@yandex.ru>
2026-05-25 22:53:35 +0300

    Added new destinations

--- app/src/main/java/com/keikuethas/irlhideandseek/view/AppNavigation.kt
+++ app/src/main/java/com/keikuethas/irlhideandseek/view/AppNavigation.kt
@@ -1,18 +1,22 @@
 package com.keikuethas.irlhideandseek.view
 
 import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.keikuethas.irlhideandseek.view.game.GameScreen
 import com.keikuethas.irlhideandseek.view.home.HomeScreen
 import com.keikuethas.irlhideandseek.view.lobby.LobbyScreen
+import com.keikuethas.irlhideandseek.view.newgame.NewGameNavigation
+import com.keikuethas.irlhideandseek.view.newgame.NewGameScreen
+import com.keikuethas.irlhideandseek.view.newgame.settings_screens.EventsSettingsScreen
 import com.keikuethas.irlhideandseek.view.newgame.settings_screens.MapSettingsScreen
 import com.keikuethas.irlhideandseek.view.newgame.settings_screens.RolesSettingsScreen
 
 @Composable
 fun AppNavigation() {
-    val navController = rememberNavController()
+    val navController: NavHostController = rememberNavController()
 
     NavHost(navController, startDestination = Home) {
 
@@ -24,21 +28,28 @@ fun AppNavigation() {
             LobbyScreen(navController)
         }
 
-        //---
-        composable<RolesSettings> {
-            RolesSettingsScreen()
+        composable<Game> {
+            GameScreen(navController)
         }
 
+        composable<NewGameNavigation> {
+            NewGameNavigation(navController)
+        }
+
+        composable<NewGame> {
+            NewGameScreen(navController)
+        }
+
+        composable<RolesSettings> {
+            RolesSettingsScreen(navController)
+        }
 
         composable<MapSettings> {
-            MapSettingsScreen()
+            MapSettingsScreen(navController)
         }
 
-        //---
-
-        composable<Game> { backStackEntry ->
-            //val role = backStackEntry.toRoute<Game>().role
-            GameScreen(navController)
+        composable<EventSettings> {
+            EventsSettingsScreen(navController)
         }
     }
 }
