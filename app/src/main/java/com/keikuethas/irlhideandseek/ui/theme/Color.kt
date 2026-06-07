2eeac6ff132f22e9ef72df8352f517c6e5f23872
Андрей <keikuethas@yandex.ru>
2026-06-07 12:28:14 +0300

    Added surface colors for role types

--- app/src/main/java/com/keikuethas/irlhideandseek/ui/theme/Color.kt
+++ app/src/main/java/com/keikuethas/irlhideandseek/ui/theme/Color.kt
@@ -1,7 +1,7 @@
 package com.keikuethas.irlhideandseek.ui.theme
 
 import androidx.compose.ui.graphics.Color
-import com.keikuethas.irlhideandseek.RoleType
+import com.keikuethas.irlhideandseek.model.RoleType
 
 val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
@@ -11,10 +11,19 @@ val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
 
+
+// REFACTOR: theme
+
 val RoleType.color: Color
     get() = when (this) {
         RoleType.SEEKER -> Color(233, 30, 99, 255)
         RoleType.HIDER -> Color(33, 150, 243, 255)
+    }
+
+val RoleType.surfaceColor: Color
+    get() = when (this) {
+        RoleType.SEEKER -> Color(236, 176, 196, 255)
+        RoleType.HIDER -> Color(183, 215, 239, 255)
     }
 
 val BarelyGrey = Color(220, 220, 220, 255)
