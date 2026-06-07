4effee449aa53c7a4c518ac98219eab6ae8525bc
Андрей <keikuethas@yandex.ru>
2026-06-07 11:19:18 +0300

    No idea as always

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,6 +48,7 @@ android {
 dependencies {
     implementation(libs.androidx.compose.material)
     implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.foundation.layout)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
     implementation(platform(libs.androidx.compose.bom))
