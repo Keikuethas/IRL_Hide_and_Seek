fad6dcfa39cd4a3897081025dae3de6c37048d0e
Андрей <keikuethas@yandex.ru>
2026-06-09 11:22:52 +0300

    Added some colors

--- app/src/main/java/com/keikuethas/irlhideandseek/utils/ZoneColors.kt
+++ app/src/main/java/com/keikuethas/irlhideandseek/utils/ZoneColors.kt
@@ -4,23 +4,14 @@ import androidx.compose.ui.graphics.Color
 import com.keikuethas.irlhideandseek.model.ZoneType
 
 val ZoneType.strokeColor: Color get() = when(this) {
-    ZoneType.SAFE -> TODO()
-    ZoneType.DANGER -> TODO()
-    ZoneType.WARNING -> TODO()
-    ZoneType.AIRDROP -> TODO()
-    ZoneType.SNARE -> TODO()
-    ZoneType.TRAP -> TODO()
-    ZoneType.SAFE_HOUSE -> TODO()
-    ZoneType.SAFE_MANSION -> TODO()
+    ZoneType.SAFE -> Color(3, 169, 244, 255)
+    ZoneType.DANGER -> Color(233, 30, 30, 255)
+    ZoneType.WARNING -> Color(255, 152, 0, 255)
+    ZoneType.AIRDROP -> Color(255, 235, 59, 255)
+    ZoneType.SNARE -> Color(126, 124, 124, 255)
+    ZoneType.TRAP -> Color(0, 0, 0, 255)
+    ZoneType.SAFE_HOUSE -> Color(0, 0,0)
+    ZoneType.SAFE_MANSION -> Color(0, 0,0)
 }
 
-val ZoneType.fillColor: Color get() = when(this) {
-    ZoneType.SAFE -> TODO()
-    ZoneType.DANGER -> TODO()
-    ZoneType.WARNING -> TODO()
-    ZoneType.AIRDROP -> TODO()
-    ZoneType.SNARE -> TODO()
-    ZoneType.TRAP -> TODO()
-    ZoneType.SAFE_HOUSE -> TODO()
-    ZoneType.SAFE_MANSION -> TODO()
-}
+val ZoneType.fillColor: Color get() = this.strokeColor.copy(alpha = 0.05F)
